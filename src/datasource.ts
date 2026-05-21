425d3b70419dbee81d1b262081587ad5685bd1a2
Kaan <kaantopkaya32@gmail.com>
2026-05-21 17:49:46 +0200

    feat: exo 2 je crois

---
+++ src/datasource.ts
@@ -0,0 +1,10 @@
+import { DataSource } from 'typeorm';
+import {User} from "./entities/User";
+
+export const AppDataSource = new DataSource({
+  type: 'better-sqlite3',
+  database: 'database.sqlite',
+  synchronize: true,
+  logging: true,
+  entities: [User],
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import 'reflect-metadata';
+import http from 'http';
 import express from 'express';
+import { AppDataSource } from './datasource';
+import { User } from './entities/User';
 
 const app = express();
 const PORT = 3000;
@@ -7,4 +11,13 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.json({ message: 'Hello world' }));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.post('/users', async (req, res) => {
+  const { first_name, last_name, email, password } = req.body;
+  const user = AppDataSource.getRepository(User).create({ first_name, last_name, email, password });
+  const result = await AppDataSource.getRepository(User).save(user);
+  res.status(201).json(result);
+});
+
+AppDataSource.initialize().then(() => {
+  http.createServer(app).listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
